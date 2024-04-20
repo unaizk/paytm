0462266e68bbead865c02e5b8e9a5cc37e669fca
root <root@Unaislaptop>
2024-04-20 18:21:40 +0530

    updated balances and onRamptransaction using transaction method

--- apps/backend-webhook/src/index.ts
+++ apps/backend-webhook/src/index.ts
@@ -1,13 +1,55 @@
 import express from "express";
-
+import db from "@repo/db/client"
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
+app.post("/hdfcWebhook", async(req, res) => {
     
-    const paymentInformation = {
+    const paymentInformation : {
+        token : string,
+        userId : string,
+        amount : string
+    } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    // Update balance in db, add txn
+
+
+    // Update balance in db, add transaction
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where : {
+                    userId : Number(paymentInformation.userId)
+                },
+                data : {
+                    amount : {
+                        increment : Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where : {
+                    token : paymentInformation.token
+                },
+                data : {
+                    status : 'Success'
+                }
+            })
+        ])
+
+        res.json({mesaage : "Captured"})
+    } catch (error) {
+        console.log(error);
+        res.status(411).json({
+            message : "Error while processing webhook"
+        })
+        
+    }
+
+   
 })
+
+
+
+app.listen(3003);
